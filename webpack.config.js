39c9dc7e9e5eb095a10ac678e2555eaeb5d9b368
James O'Reilly <James.Oreilly926@gmail.com>
2018-02-25 14:57:37 -0500

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './client/index.js',
+    output: {
+      path: __dirname,
+      filename: './public/bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015']
+          }
+        }
+      ]
+    }
+  };
