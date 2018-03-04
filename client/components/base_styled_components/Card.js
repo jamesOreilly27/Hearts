04f4472fddbaac626867065773a151f12e23fea5
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-04 12:50:34 -0500

    chore: create CardContainer styled component in Card component

--- client/components/base_styled_components/Card.js
+++ client/components/base_styled_components/Card.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const CardContainer = styled.div`
+	height: 8em;
+	width: 3em;
+	border: 2px solid black;
+	text-align: center;
+`
+
 const Card = ({ card }) => (
-	<div>
-		{card.values.renderValue}
-		{card.suit}
-	</div>
+	<CardContainer>
+		<div>
+			{card.values.renderValue}
+		</div>
+		<div>
+			{card.suit}
+		</div>
+	</CardContainer>
 )
 
 
