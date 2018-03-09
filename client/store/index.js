d41a4695dc012d603de874d01b8f28bd34d99024
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-09 09:19:46 -0500

    import redux tools

---
+++ client/store/index.js
@@ -0,0 +1,5 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
