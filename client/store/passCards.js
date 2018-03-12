cdafe690279f7bf193edbf9f9b92e3dd6ec837d7
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-12 01:58:18 -0400

    add redux architecture for clearing the passcards array in client/store/passCards.js

--- client/store/passCards.js
+++ client/store/passCards.js
@@ -1,6 +1,9 @@
+/***** ACTION TYPE VARIABLES *****/
 const ADD_PASSCARD = 'ADD_PASSCARD'
 const REMOVE_PASSCARD = 'REMOVE_PASSCARD'
+const RESET_PASSCARDS = 'RESET_PASSCARDS'
 
+/***** ACTION CREATORS*****/
 const selectPasscard = card => ({
   type: ADD_PASSCARD,
   payload: card
@@ -11,6 +14,12 @@ const deselectPasscard = card => ({
 	payload: card
 })
 
+const resetPasscards = () => ({
+  type: RESET_PASSCARDS,
+  payload: []
+})
+
+/***** THUNKS *****/
 export const newPasscardThunk = card => dispatch => {
   dispatch(selectPasscard(card))
 }
@@ -19,12 +28,18 @@ export const undoPasscardThunk = card => dispatch => {
 	dispatch(deselectPasscard(card))
 }
 
+export const clearPasscardsThunk = () => dispatch => {
+  dispatch(resetPasscards())
+}
+
 const reducer = (passCards = [], action) => {
   switch (action.type) {
     case ADD_PASSCARD:
 			return [...passCards, action.payload]
 		case REMOVE_PASSCARD:
-			return passCards.filter(passCard => passCard !== action.payload)
+      return passCards.filter(passCard => passCard !== action.payload)
+    case RESET_PASSCARDS:
+      return action.payload
     default:
       return passCards
   }
