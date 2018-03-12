9ebd5b09b438ae003b4a32a2be208687ea8aad49
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-12 01:11:18 -0400

    finish passCards function in client/utils/passingCards.js

--- client/utils/passingCards.js
+++ client/utils/passingCards.js
@@ -50,10 +50,41 @@ const setAllPassAndHoldCards = (userHand, userPassCards, compHandsArray) => {
 	}
 }
 
-const passLeft = (userPassCards, userHand, comp1Hand, comp2Hand, comp3Hand) => {
+// PassCards function will make sure each player keeps his/her hold cards
+// and that his/her pass cards are sent to another player
+// depending on what hand it is the pass cards are moved to a different player
+// 	-First hand cards go to the left
+// 	-Second Hand cards go to the right
+// 	-Third Hand cards go across
+const passCards = (handCount, userPassCards, userHand, comp1Hand, comp2Hand, comp3Hand) => {
 	const compHandsArray = [comp1Hand, comp2Hand, comp3Hand]
 	const holdAndPass = setAllPassAndHoldCards(userHand, userPassCards, compHandsArray)
-	console.log('TESTING', holdAndPass)
+	const passWhereInt = handCount % 3
+	const user, comp1, comp2, comp3
+
+	if(passWhereInt === 0) {
+		user = holdAndPass.holdCards.user.concat(holdAndPass.passCards.comp3)
+		comp1 = holdAndPass.holdCards.comp1.concat(holdAndPass.passCards.user)
+		comp2 = holdAndPass.holdCards.comp2.concat(holdAndPass.passCards.comp1)
+		comp3 = holdAndPass.holdCards.comp3.concat(holdAndPass.passCards.comp2)
+	} else if(passWhereInt === 1) {
+		user = holdAndPass.holdCards.user.concat(holdAndPass.passCards.comp1)
+		comp1 = holdAndPass.holdCards.comp1.concat(holdAndPass.passCards.comp2)
+		comp2 = holdAndPass.holdCards.comp2.concat(holdAndPass.passCards.comp3)
+		comp3 = holdAndPass.holdCards.comp3.concat(holdAndPass.passCards.user)
+	} else if(passWhereInt === 2) {
+		user = holdAndPass.holdCards.user.concat(holdAndPass.passCards.comp2)
+		comp1 = holdAndPass.holdCards.comp1.concat(holdAndPass.passCards.comp3)
+		comp2 = holdAndPass.holdCards.comp2.concat(holdAndPass.passCards.user)
+		comp3 = holdAndPass.holdCards.comp3.concat(holdAndPass.passCards.comp1)
+	}
+
+	return {
+		user,
+		comp1,
+		comp2,
+		comp3
+	}
 }
 
-export default passLeft
+export default passCards
