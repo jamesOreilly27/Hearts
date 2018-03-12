97da9c543193cb2bfb1880c495ab163c08d03f93
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-12 01:18:17 -0400

    add redux architecture for passing cards in client/store/hands.js

--- client/store/hands.js
+++ client/store/hands.js
@@ -1,17 +1,28 @@
+/***** ACTION TYPE VARIABLES*****/
 const DEAL_CARDS = 'DEAL_CARDS'
+const PASS_CARDS = 'PASS_CARDS'
 
+/***** ACTION CREATORS *****/
 const dealCards = hands => ({
   type: DEAL_CARDS,
   payload: hands
 })
 
-export const dealCardsThunk = hands => dispatch => {
-  dispatch(dealCards(hands))
-}
+const passCards = hands => ({
+  type: PASS_CARDS,
+  payload: hands
+})
+
+/***** THUNKS *****/
+export const dealCardsThunk = hands => dispatch => dispatch(dealCards(hands))
+export const passCardsThunk = hands => dispatch => dispatch(passCards(hands))
+
 
 const reducer = (hands = {}, action) => {
   switch (action.type) {
     case DEAL_CARDS:
+      return action.payload
+    case PASS_CARDS:
       return action.payload
     default:
       return hands
