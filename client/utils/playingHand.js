5f97e5011b639f1d43fd25f8b137fd2695690b6e
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-13 12:36:09 -0400

    add function to locate the two of clubs

---
+++ client/utils/playingHand.js
@@ -0,0 +1,16 @@
+//The first card played in every hand is the two of clubs
+//This function will find who has that card after passing is complete
+//Our hands are sorted so the 2 of clubs will be the first card in whatever array holds it
+
+const findTwoOfClubs = hands => {
+    let hasTwo;
+    const { user, comp1, comp2, comp3 } = hands
+    const cards = [user[0], comp1[0], comp2[0], comp3[0]]
+    const players = ['user', 'comp1', 'comp2', 'comp3']
+    
+    for(let i = 0; i < cards.length; i++) {
+        const card = cards[i]
+        if(card.suit === 'Clubs' && card.values.renderValue === '2') hasTwo = players[i]
+    }
+    return hands[hasTwo]
+}
